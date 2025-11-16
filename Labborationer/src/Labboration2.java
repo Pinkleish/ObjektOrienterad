037a7f4c36a974ef2fc84cc03ef15b7ef49a5002
Pinkleish <axel.dahl@hotmail.se>
2025-11-16 13:54:26 +0100

    Lab2 färdig

--- Labborationer/src/Labboration2.java
+++ Labborationer/src/Labboration2.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class Labboration2 {
     public static void main(String[] args) {
@@ -10,11 +11,12 @@ public class Labboration2 {
 //uppgift04(input);
 //uppgift05(input);
 //uppgift06();
-uppgift07();
+//uppgift07();
 //uppgift08();
 //uppgift09();
-//uppgift10();
+//uppgift10(input);
 //uppgift11();
+//uppgift12();
     }
 
     public static void uppgift01() {
@@ -112,20 +114,101 @@ uppgift07();
         System.out.println("Uppgift 08");
         int[][] grid = new int[13][13];
 // Skriv din kod här
-        System.out.println();
+        for (int i = 0; i < grid.length;i++){
+            for (int j = 0; j < grid[i].length;j++){
+                grid[i][j] = i*j;
+                System.out.print(grid[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+
     }
 
     public static void uppgift09() {
         System.out.println("Uppgift 09");
 // Skriv din kod här
-        System.out.println();
+        int sum1 = IntAddition(1,2);
+        double sum2 = DoubleAddition(1.5,2.5);
+        double sum3 = intAndDouble(1,2.5);
+        System.out.println(sum1 + " " + sum2 + " " + sum3);
+        System.out.println(intAndDouble(sum1,sum2));
+
+        }
+    public static int IntAddition(int x,int y){
+        int sum = x + y;
+        return sum;
+    }
+    public static double DoubleAddition(double x, double y){
+        double sum = x + y;
+        return sum;
+    }
+    public static double intAndDouble(int x, double y){
+        double sum = x + y;
+        return sum;
     }
 
-    public static void uppgift10() {
+    public static void uppgift10(Scanner input) {
         System.out.println("Uppgift 10");
 // Skriv din kod här
+        String b = "Admin";
+        String a = input.next();
+        if (a.equals(b)){
+            System.out.println("Welcome");
+        }
+        else if ((a.toLowerCase()).equals(b.toLowerCase())){
+            System.out.println("Almost");
+        }
+        else{
+            System.out.println("Unwelcome");
+        }
 
     }
+    public static void uppgift11(){
+        System.out.println("Uppgift 11");
+// Skriv din kod här
+        int[] arr = new int[]{4, 6, 8, 2, 1, 3};
+        int n = arr.length;
+        int temp = -1;
+        for (int i=0; i<n ; i++){
+            for (int j=0; j<(n-1); j++){
+                if (arr[j] > arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for (int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+    public static void uppgift12(){
+        System.out.println("Uppgift 12");
+// Skriv din kod här
+        double[] mean_stars = new double[]{0.8,0.55,0.75};
+        double[] mean_galaxies = new double[]{0.3,0.55,0.25};
+        double[] body = new double[]{1.0,1.0,1.0};
+
+        // Koordinater
+        double a1 = body[0];
+        double a2 = body[1];
+        double a3 = body[2];
+
+        double b1 = mean_galaxies[0];
+        double b2 = mean_galaxies[1];
+        double b3 = mean_galaxies[2];
+
+        double distance1 = Math.pow(a1-b1,2);
+        double distance2 = Math.pow(a2-b2,2);
+        double distance3 = Math.pow(a3-b3,2);
+
+        double distance = Math.sqrt(distance1+distance2+distance3);
+        System.out.println(distance);
+        }
+
+
 }
 
 
