e50c5c86e21e2deb64db4874eec168e3a80c1a97
Pinkleish <axel.dahl@hotmail.se>
2025-11-18 15:38:41 +0100

    Anteckningar4

---
+++ Anteckningar/Anteckningar4/Anteckningar4.java
@@ -0,0 +1,5 @@
+public class Anteckningar4 {
+    public static void Main(String[] args){
+
+    }
+}
---
+++ Anteckningar/Anteckningar4/Breed.java
@@ -0,0 +1,10 @@
+public enum Breed {
+    GOLDENRETRIEVER("Golden retriever"),
+    BORDERCOLLIE("Border Collie");
+
+    //private final String breedName;
+
+    Breed(String n){
+            
+    }
+}
